7f329417f90137895f0f8ca1250ad2876d4c02bb
hemspraxis <hems@praxis.com.mx>
2016-01-13 12:01:14 -0600

    tiempos

--- src/test/java/com/praxis/bisnet/test/pages/main/viaticos/comprobaciones/AutorizarComprobacionPage.java
+++ src/test/java/com/praxis/bisnet/test/pages/main/viaticos/comprobaciones/AutorizarComprobacionPage.java
@@ -60,9 +60,9 @@ public class AutorizarComprobacionPage extends Page<AutorizarComprobacionPage> {
 		try {
 			waitForWebElementDisplayed(folioColumn);
 			safeClick(folioColumn);
-			Thread.sleep(2000);
+			Thread.sleep(10000);
 			safeClick(folioColumn);
-			Thread.sleep(2000);
+			Thread.sleep(10000);
 			safeClick(folioRow);
 			for (int i = 1; i <= 3; i++) {
 				waitForWebElementDisplayed(tBody);

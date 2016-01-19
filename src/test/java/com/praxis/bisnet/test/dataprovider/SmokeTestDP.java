47e405ec779f0a3bf5a1b3255fe4c10066914824
hemspraxis <hems@praxis.com.mx>
2016-01-19 14:03:25 -0600

    19/01/2016 14:03:00

--- src/test/java/com/praxis/bisnet/test/dataprovider/SmokeTestDP.java
+++ src/test/java/com/praxis/bisnet/test/dataprovider/SmokeTestDP.java
@@ -6,28 +6,28 @@ import static com.praxis.bisnet.test.dataprovider.DataProviderReferencesConstant
 public class SmokeTestDP {
 
 	@DataProvider(name = SIGNON)
-	public static Object [][] signonProvider(){
+	public static Object [][] signonProvider() {
 		return new Object[][] {
 				{"crca@praxis.com.mx", "123456"}
 		};
 	}
 
 	@DataProvider(name = SIGNON_ADMINCC)
-	public static Object [][] signonForAdministrador(){
+	public static Object [][] signonForAdministrador() {
 		return new Object[][] {
 				{"oraj@praxis.com.mx", "123456"}
 		};
 	}
 	
 	@DataProvider(name = SIGNON_ADMINVIA)
-	public static Object [][] signonForCotizacion(){
+	public static Object [][] signonForCotizacion() {
 		return new Object[][] {
 				{"ragm@praxis.com.mx", "123456"}
 		};
 	}
 	
 	@DataProvider(name = SIGNON_TESORERO)
-	public static Object [][] signonForEstadoCuenta(){
+	public static Object [][] signonForEstadoCuenta() {
 		return new Object[][] {
 				{"sare@praxis.com.mx", "123456"}
 		};
